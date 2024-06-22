c912a7a84fd41ba204ce4246815a019da0bf7d07
giridharansivam <54465468+giridharansivam@users.noreply.github.com>
2024-06-22 09:01:56 -0400

    Delete src directory

--- src/containers/index.js
+++
@@ -1,3 +0,0 @@
-export  {default as Builder } from'./builder/Builder'
-export  {default as Footer }from'./footer/Footer'
-export  {default as Header }from'./header/Header'
--- src/pages/index.js
+++
@@ -1,1 +0,0 @@
-export  {default as resumeBuilder } from'./resumeBuilder/resumeBuilder'
